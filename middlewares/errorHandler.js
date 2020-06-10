4c12b3c146caab23ad1306293a625061b1038a03
nicolv <lvxiaohui@sou.edu.cn>
2020-06-10 14:07:24 +0800

    update

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,6 +1,18 @@
 const errorHandler = {
   error(app, logger) {
     app.use(async (ctx, next) => {
+      
+      try {
+        await next();
+      } catch (error) {
+        logger.error(error)
+        ctx.status = ctx.status || '500';
+        ctx.body = '500啦😭'
+      }
+
+    })
+    
+    app.use(async (ctx, next) => {
 
       await next();
 
@@ -8,7 +20,7 @@ const errorHandler = {
         return
       }
 
-      ctx.status = 404;
+      ctx.status = 404; // 有些网站会重新生成200，是为了防止百度会降权；
       ctx.body = `<script type="text/javascript" src="//qzonestyle.gtimg.cn/qzone/hybrid/app/404/search_children.js" charset="utf-8"></script>`
     })
   }
