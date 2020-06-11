988d7aaa49ca2d09d09e268457d92113d2c7ed5f
nicolv <lvxiaohui@sou.edu.cn>
2020-06-11 14:46:11 +0800

    use nomodule

---
+++ assets/scripts/index-bundle.js
@@ -0,0 +1,19 @@
+System.register([], function (_export, _context) {
+  "use strict";
+
+  var data;
+  return {
+    setters: [],
+    execute: function () {
+      data = '🐷 index';
+
+      _export("default", data); // new Vue({
+      //   el: '#app-6',
+      //   data: {
+      //     message: 'Hello Vue'
+      //   }
+      // })
+
+    }
+  };
+});
