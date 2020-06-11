f16b52a96f86ce1e5507b1e99c60698c6a478255
nicolv <lvxiaohui@sou.edu.cn>
2020-06-11 09:46:24 +0800

    使用函数式编程

---
+++ assets/scripts/demo.js
@@ -0,0 +1,59 @@
+(function() {
+
+  var root = typeof self == 'object' && self.self === self && self ||
+    typeof global == 'object' && global.global === global && global ||
+    Function('return this')() || {};
+
+  var ArrayProto = Array.prototype;
+  var push = ArrayProto.push;
+
+  function _(obj) {
+    if (!(this instanceof _)) {
+      return new _(obj)
+    }
+
+    this._wrapped = obj;
+  }
+
+  _.isFunction = function(obj) {
+    return typeof obj == 'function' || false;
+  };
+
+  _.functions = function(obj) {
+    var names = [];
+    for (var key in obj) {
+      if (_.isFunction(obj[key])) names.push(key);
+    }
+    return names.sort();
+  }
+
+  _.each = function(obj, callback) {
+    if (Array.isArray(obj)) {
+      for (let item of obj) {
+        callback && callback.call(_, item)
+      }
+    }
+  }
+
+  _.map = function(wrapped, callback) {
+    console.log("map")
+    console.log("🍊", wrapped)
+    console.log("🍎", callback)
+  }
+
+  _.mixin = function(obj) {
+    _.each(_.functions(obj), function(name) {
+      var func = _[name] = obj[name];
+      console.log('🐛', name)
+
+      _.prototype[name] = function() {
+        push.apply([this._wrapped], arguments);
+        return func.apply(_, [this._wrapped]);
+      };
+    });
+    return _;
+  }
+  _.mixin(_)
+
+  root._ = _
+})();
